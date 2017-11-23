226389abf3ad24519e0608342acfa93d79c93423
Rahul Gautam <deltaman2000@gmail.com>
2017-11-23 11:52:33 -0600

    Final Commit for First APP

---
+++ FristApp/app/src/main/java/com/firstapp/rgautam/firstapp/MyWorker.java
@@ -0,0 +1,11 @@
+package com.firstapp.rgautam.firstapp;
+
+/**
+ * Created by Deltaman on 11/23/17.
+ */
+
+public class MyWorker {
+    public static int doubleTheValue(int value){
+        return 2*value;
+    }
+}
